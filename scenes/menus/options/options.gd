a5cbefe70a6ee3ad3cb226eae7fe6cfa989fd4ab
DeceivingPanda <zbowen@null.net>
2024-12-28 05:26:36 -0600

    Start to options menu, and test of 3D model space

---
+++ scenes/menus/options/options.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$lblMouseSenseAmount.text = str(Save.game_data.mouse_sens / -0.001)
+	$sliderMouseSensitivity.value = (Save.game_data.mouse_sens / -0.001)
+	GSettings.toggle_fullscreen(Save.game_data.fullscreen_on)
+	$ChkFullScreen.button_pressed = Save.game_data.fullscreen_on
+	$sliderMasterVol.value = Save.game_data.master_vol
+	$sliderSFXVol.value = Save.game_data.sfx_vol
+	$sliderMusicVol.value = Save.game_data.music_vol
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+	
+func _on_chk_full_screen_toggled(toggled_on: bool) -> void:
+	#pass toggle state to global settings
+	
+	GSettings.toggle_fullscreen(true if toggled_on == true else false)
+
+
+func _on_slider_mouse_sensitivity_value_changed(value: float) -> void:
+	GSettings.updateMouseSense(value)
+	$lblMouseSenseAmount.text = str(value)
+
+func _on_slider_master_vol_value_changed(value: float) -> void:
+	GSettings.updateMasterVol(value)
+
+
+func _on_slider_music_vol_value_changed(value: float) -> void:
+	GSettings.updateMusicVol(value)
+
+
+func _on_slider_sfx_vol_value_changed(value: float) -> void:
+	GSettings.updateSFXVol(value)

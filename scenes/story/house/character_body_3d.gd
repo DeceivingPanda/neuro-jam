34254003cb789eb842c99402491e12476aa24307
Lenduroido <75614767+Lenduroido@users.noreply.github.com>
2024-12-29 14:21:40 +0800

    Camera movement update

--- scenes/story/house/character_body_3d.gd
+++ scenes/story/house/character_body_3d.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+@onready var head := $Head
+@onready var camera := $Head/Camera3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -22,18 +24,26 @@ func _physics_process(delta: float) -> void:
 	if $"..".ControlLock == true:
 		pass
 	else:
-		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if direction:
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.z = move_toward(velocity.z, 0, SPEED)
-		
 		move_and_slide()
+
 func _input(event):
-		if event is InputEventMouseMotion:
-			if $"..".ControlLock == true:
-				pass
-			else:
-				rotate(global_transform.basis.y.normalized(), event.relative.x * Save.game_data.mouse_sens)
+	if $"..".ControlLock == true:
+		pass
+	else:
+		if event is InputEventMouseButton:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		elif event.is_action_pressed('ui_cancel'):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+		if Input.MOUSE_MODE_CAPTURED:
+			if event is InputEventMouseMotion:
+				head.rotate_y(event.relative.x * Save.game_data.mouse_sens)
+				camera.rotate_x(event.relative.y * Save.game_data.mouse_sens)
+				camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-65), deg_to_rad(100))

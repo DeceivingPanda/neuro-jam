36a41a1caa36d1fae7ab46ef62ffdbedfa7c3849
dragon-masterk <83424830+dragon-masterk@users.noreply.github.com>
2024-12-30 00:32:56 -0500

    (feat): Platformer
    - On player lose, reload house scene
    - On player win, restart level

--- scenes/minigames/platformer/platformer.gd
+++ scenes/minigames/platformer/platformer.gd
@@ -8,24 +8,23 @@ func _ready() -> void:
 	add_child(current_level)
 	if current_level.has_signal("player_win"):
 		var win:Signal = current_level.player_win
-		win.connect(_on_level_player_win,4)
+		win.connect(_on_level_player_win)
 		#print(win.get_connections())
 	if current_level.has_signal("player_lose"):
 		var lose:Signal = current_level.player_lose
-		lose.connect(_on_level_player_lose,4)
-		#print(lose.get_connections())
+		lose.connect(_on_level_player_lose)
+		print(lose.get_connections())
 
 
 func _on_level_player_win(level: int) -> void:
-	print('player won in level %s' % level)
-	
+	#print('player won in level %s' % level)
 	#start timer that will loop the level
 	$RestartTimer.start(2)
 
 
 func _on_level_player_lose(level: int) -> void:
 	print('player lost in level %s' % level)
-	$RestartTimer.start(2)
+	get_tree().change_scene_to_file("res://scenes/story/house/house.tscn")
 
 
 func _on_timer_timeout() -> void:

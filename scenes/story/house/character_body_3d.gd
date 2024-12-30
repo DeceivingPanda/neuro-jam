a5e98cdc5b071d250db30ef925999ffbd1bc25f7
DeceivingPanda <zbowen@null.net>
2024-12-30 04:20:26 -0600

    stairs and objects

--- scenes/story/house/character_body_3d.gd
+++ scenes/story/house/character_body_3d.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 class_name Vedal
 @onready var head := $Head
 @onready var camera := $Head/Camera3D
-var gamestage = 0
+var gamestage = 10
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const SPRINT_VELOCITY = 2
